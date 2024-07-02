5c0c5862cb9bde8c211002d606a4e8d1c624b961
danyow-cheung <17702069165@163.com>
2024-07-02 18:06:26 +0800

    0702

--- config/info.py
+++ config/info.py
@@ -2,12 +2,14 @@ import json
 import requests 
 from openai import OpenAI
 import os 
+import numpy as np 
 
 with open("config/config.json",encoding="utf-8") as f:
     data_json = json.load(f)
 
 
-
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 class OneAPI:
     
     def __init__(self) -> None:
@@ -25,4 +27,17 @@ class OneAPI:
             model="chatglm3-6b-251"
             )
         return chat_completion.choices[0].message.content 
+    
+    def get_embedding(self,text, model="m3e-251"):
+        text = text.replace("\n", " ")
+        return self.client.embeddings.create(input = [text], 
+        model=model).data[0].embedding
+    
+    def similarity(self,text1,text2):
+        # print('>>>对比的文本1',text1)
+        # print('>>>对比的文本2',text2)
+
+        embed1 = self.get_embedding(text1)
+        embed2 = self.get_embedding(text2)
+        return cosine_similarity(embed1,embed2)
     
