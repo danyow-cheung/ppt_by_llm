e1d8dad04e81c8e32045371f1eaa27b3e0653a41
danyow-cheung <17702069165@163.com>
2024-07-04 10:57:11 +0800

    0704

--- auto2ppt_summary.py
+++ auto2ppt_summary.py
@@ -13,10 +13,11 @@ def remove_special_characters(input_string):
     return "".join(result)
 
 class Doc2ppt:
-    def __init__(self) -> None:
+    def __init__(self,max_word_count) -> None:
         # self.max_word_count = 5000  # 最大文字数为5k-1w
-        self.max_word_count = 2000  # 最大文字数为5k-1w
-        
+        # self.max_word_count = 4000  # 最大文字数为5k-1w
+        self.max_word_count = max_word_count
+
         self.one = OneAPI()
     def read_doc(self,doc_path):
 
@@ -63,12 +64,7 @@ class Doc2ppt:
         if left < len(text_lists):
             result.append(text_lists[left])
         return result 
-    def clean_data(self):
-        #todo ：对获取到的文本进行停用词处理，过滤无效低频信息
-        pass 
-    def after_clean_data(self):
-        #todo :输入到ppt前最后的数据清洗
-        pass 
+    
 
     def llm_content(self,sub ):
         short_sub = self.one.post_v2(f"帮我将下面的输入进行文字的简化,如果输入文字超过了200字，需要简化到200字以内，{sub}")
@@ -81,6 +77,7 @@ class Doc2ppt:
     def split_summary_doc(self,text_lists):
         '''
         分割后的逻辑代码
+        20240704,最简单的文本生成ppt
         '''
         result = self.split_summary_algo(text_lists)
 
@@ -94,41 +91,81 @@ class Doc2ppt:
 
         print("分割后的文字分段",len(result))
         print('总标题',title)
-
+        recycle_generate =0 
         left = 0 
         while left < len(result):
+            print('当前',recycle_generate,"left",left)
             sub = result[left]
-
-            short_sub,short_title = self.generate_loop(sub)
-            # 如果不满足条件,继续调用 self.generate_loop() 直到满足条件
-            while len(short_title) < 10 or len(short_sub) <= 250:
-                short_sub,short_title = self.generate_loop(sub)
-            
+            short_sub,short_title = self.generate_loop_v2(sub) 
             left +=1 
             ready_list.append(short_sub)
             ready_title.append(short_title)
 
-        print("len(ready_list)",ready_list)
-        print("len(ready_titles)",ready_title)
+        self.send2ppt(title,ready_list,ready_title,flag=True) 
 
-        self.send2ppt(title,ready_list,ready_title) #todo ： 这里的送入ppt的消息也要经过预处理才行
 
-    def generate_loop(self,sub):
+    def content_loop(self,sub):
         short_sub = self.llm_content(sub)
+        pure_short_sub = remove_special_characters(short_sub)
+        return short_sub,pure_short_sub 
+    def title_loop(self,sub):
         short_title = self.llm_title(sub)        
-        short_sub = remove_special_characters(short_sub)
-        # short_title=remove_special_characters(short_title)
-        return short_sub,short_title
+        pure_short_title=remove_special_characters(short_title)
+        return short_title,pure_short_title 
     
+    
+    def generate_loop_v2(self, sub):
+        while True:
+            short_sub, pure_short_sub = self.content_loop(sub)
+            short_title, pure_short_title = self.title_loop(sub)
+
+            print(f'当前 len(pure_short_sub)={len(pure_short_sub)}, len(pure_short_title)={len(pure_short_title)}')
+
+            if len(pure_short_sub) <= 250 and len(pure_short_title) <= 20:
+                return short_sub, short_title
 
 
-    def send2ppt(self,topic:str,content_list:list,title_list:list):
 
-        #todo:auto ppt generate 
+    def send2ppt(self,topic:str,content_list:list,title_list:list,flag:bool,image_list:list):
         from ppt.sub import PptGenerator
         ppt = PptGenerator()
-        ppt.pure_text_generate(topic,content_list,title_list)
+        if flag==True:
 
+            ppt.pure_text_generate(topic,content_list,title_list)
+        else:
+            # todo: 图文匹配的ppt
+            ppt.image_text_generate(topic,content_list,title_list,image_list)
+
+
+    def split_sumary_doc_with_imgs(self,text_lists,images_list):
+        '''
+        分割后的逻辑代码
+        20240704,最简单的文本生成ppt
+        '''
+        result = self.split_summary_algo(text_lists)
+
+        ready_text ={"topic":None,"content_list":None,"sub_title":None}
+        ready_list = []
+        ready_title=[]
+        
+        title = self.one.post_v2(f"阅读下面的文字，然后输出标题 文字：{text_lists}")
+        ready_text['topic'] = title 
+        title = title[3:] 
+
+        print("分割后的文字分段",len(result))
+        print('总标题',title)
+        recycle_generate =0 
+        left = 0 
+        while left < len(result):
+            print('当前',recycle_generate,"left",left)
+            sub = result[left]
+            short_sub,short_title = self.generate_loop_v2(sub) 
+            left +=1 
+            ready_list.append(short_sub)
+            ready_title.append(short_title)
+        
+        flag=  False 
+        self.send2ppt(title,ready_list,ready_title,flag,images_list)
 
 if __name__ =="__main__":
     pass 
--- ppt/sub.py
+++ ppt/sub.py
@@ -3,102 +3,46 @@ import time
 from pptx.util import Inches,Cm 
 import random 
 
-
-def Text2Img(ppt,text_content,image_path,index):
-    '''
-    左边为文字，右边为图片'''
-    # second 
-    slide=ppt.slides.add_slide(ppt.slide_layouts[1])
-    slide.placeholders[0].text=str(index) 
-    sub_title=slide.placeholders[1].text_frame.add_paragraph() # 这个是防止出现多余的文本输入框来的
-    # 添加左侧文字占位符
-    #! left_box = slide.shapes.add_textbox(Inches(0.5), Inches(2), Inches(6), Inches(4)) 在幻灯片上创建了一个宽 6 英寸、高 4 英寸的文本框,它距离幻灯片左边缘 0.5 英寸、顶边缘 2 英寸的位置。
-    left_box = slide.shapes.add_textbox(Inches(0.5), Inches(2), Inches(4), Inches(4))
-    left_text_frame = left_box.text_frame
-    left_text_frame.word_wrap = True# 自动换行
-    
-
-    left_text_frame.text = text_content 
-    # 添加右侧图片占位符
-    #! 距左边缘 7 英寸、距顶边缘 2 英寸的图片,图片大小为宽 4 英寸、高 4 英寸。
-    image_path = rf"{image_path}"
-    right_box = slide.shapes.add_picture(image_path, Inches(5), Inches(2), width=Inches(4), height=Inches(4))
-    
-    return slide 
-
-
-def Img2Text(ppt,text_content,image_path,index):
-    slide=ppt.slides.add_slide(ppt.slide_layouts[1])
-    slide.placeholders[0].text=str(index)
-    sub_title=slide.placeholders[1].text_frame.add_paragraph() # 这个是防止出现多余的文本输入框来的
-    
-    image_path = rf"{image_path}"
-    left_box = slide.shapes.add_picture(image_path, Inches(0.5), Inches(2), width=Inches(4), height=Inches(4))
-
-    # 添加右侧图片占位符
-    #! 距左边缘 7 英寸、距顶边缘 2 英寸的图片,图片大小为宽 4 英寸、高 4 英寸。
-    right_box =slide.shapes.add_textbox(Inches(5), Inches(2), Inches(4), Inches(4))
-    right_box_frame = right_box.text_frame
-    right_box_frame.word_wrap = True # 自动换行
-
-
-    right_box_frame.text = text_content
-    return slide 
-
-def random_concate(main_topic,images_list,text_list):
-
-    ppt=Presentation()
-    slide=ppt.slides.add_slide(ppt.slide_layouts[0])
-    slide.placeholders[0].text=main_topic if main_topic else "未命名"
-    sub_title=slide.placeholders[1].text_frame.add_paragraph() 
-
-    for idx,value in enumerate(zip(images_list,text_list)):
-        image,text = value 
-        print(idx,image,text)
-        
-                
-        if idx % 2 == 0:
-            slide = Text2Img(ppt,text,image,idx)
-        else:
-            # pass # Odd
-            slide = Img2Text(ppt,text,image,idx)
-
-    slide=ppt.slides.add_slide(ppt.slide_layouts[0])
-    slide.placeholders[0].text="多谢观看"
-    slide.placeholders[1].text="author:danyow"
-
-    ppt.save(f'output/Test_{int(time.time())}.pptx')
     
 class PptGenerator:
     def __init__(self) -> None:
         self.ppt=Presentation()
 
-    def pure_text_generate(self,topic:str,content_list:list,title_list:list):
-
+    def welcome_page(self,topic):
         slide=self.ppt.slides.add_slide(self.ppt.slide_layouts[0]) # title&subtitle layout
         # 首页
         slide.placeholders[0].text=topic
         slide.placeholders[1].text="author:danyow"
+        return slide 
+    
+    def end_page(self):
+        # 结束页
+        slide=self.ppt.slides.add_slide(self.ppt.slide_layouts[0]) 
+        slide.placeholders[0].text="Thanks for watching "
+        slide.placeholders[1].text="author:danyow"
+        return slide 
+    def pure_text_generate(self,topic:str,content_list:list,title_list:list):
+
+        # 首页
+        slide = self.welcome_page(topic)
         # 内容页
-        # slide = self.ppt.slides.add_slide(self.ppt.slide_layouts[9])
-        
+
         for i,page in enumerate(zip(content_list,title_list)):
             content = page[0]            
             title = page[1]
             slide=self.ppt.slides.add_slide(self.ppt.slide_layouts[1])
 
-            first = slide.placeholders[0].text=title 
+            slide.placeholders[0].text=title 
             second = slide.placeholders[1].text_frame.add_paragraph()
             second.text = content 
             second.word_wrap=True
 
 
         # 结束页
-        slide=self.ppt.slides.add_slide(self.ppt.slide_layouts[0]) 
-        slide.placeholders[0].text="Thanks for watching "
-        slide.placeholders[1].text="author:danyow"
+        slide = self.end_page()
 
-        self.ppt.save(f'output/{topic}.pptx')
+
+        self.ppt.save(f"output/{int(time.time())}_{topic}.pptx")
 
 
         print("ppt生成完成")
@@ -109,3 +53,63 @@ class PptGenerator:
         slide.shapes._spTree.insert(2, pic._element)
         return slide 
 
+    
+    def image_text_generate(self,topic:str,content_list:list,title_list:list,images_list:list,image_list:list):
+        
+        # 首页
+        slide = self.welcome_page(topic)
+        #! 下面的情况是图片和文字长度相同
+        if len(image_list)==len(content_list):
+            slide = self.same_length_ppt(images_list,content_list,title_list)
+        
+        elif len(images_list)<len(content_list):
+            raise NotImplementedError 
+        
+        else:
+            pass 
+
+        # 结束页
+        slide = self.end_page()
+        self.ppt.save(f"output/{int(time.time())}_{topic}.pptx")
+
+    
+    def same_length_ppt(self,images_list,content_list,title_list):
+        for idx,value in enumerate(zip(images_list,content_list,title_list)): 
+            image,text,title = value            
+            if idx % 2 == 0:
+                slide = self.Text2Img(text,image,title)
+            else:
+                # Odd
+                slide = Img2Text(text,image,title)
+        
+        return slide 
+    
+    def Text2Img(self,text_content,image_path,title):
+        '''
+        左边为文字，右边为图片'''
+        # second 
+        slide=self.ppt.slides.add_slide(self.ppt.slide_layouts[1])
+        slide.placeholders[0].text=title  
+        sub_title=slide.placeholders[1].text_frame.add_paragraph() # 这个是防止出现多余的文本输入框来的
+        left_box = slide.shapes.add_textbox(Inches(0.5), Inches(2), Inches(4), Inches(4))
+        left_text_frame = left_box.text_frame
+        left_text_frame.word_wrap = True# 自动换行
+        left_text_frame.text = text_content 
+        image_path = rf"{image_path}"
+        right_box = slide.shapes.add_picture(image_path, Inches(5), Inches(2), width=Inches(4), height=Inches(4))
+        
+        return slide 
+
+    def Img2Text(self,text_content,image_path,title):
+        slide=self.ppt.slides.add_slide(self.ppt.slide_layouts[1])
+        slide.placeholders[0].text=title 
+        sub_title=slide.placeholders[1].text_frame.add_paragraph() # 这个是防止出现多余的文本输入框来的
+        image_path = rf"{image_path}"
+        left_box = slide.shapes.add_picture(image_path, Inches(0.5), Inches(2), width=Inches(4), height=Inches(4))
+        # 添加右侧图片占位符
+        #! 距左边缘 7 英寸、距顶边缘 2 英寸的图片,图片大小为宽 4 英寸、高 4 英寸。
+        right_box =slide.shapes.add_textbox(Inches(5), Inches(2), Inches(4), Inches(4))
+        right_box_frame = right_box.text_frame
+        right_box_frame.word_wrap = True # 自动换行
+        right_box_frame.text = text_content
+        return slide 
